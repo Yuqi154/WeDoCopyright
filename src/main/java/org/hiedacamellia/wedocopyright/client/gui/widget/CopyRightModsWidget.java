a219d99bc322108997d5ae256d5edc9d9cf1adb2
Yuqi154 <1123489751@qq.com>
2025-05-22 23:18:26 +0800

    update: increment mod version to 0.0.6 and improve logo loading error handling

--- src/main/java/org/hiedacamellia/wedocopyright/client/gui/widget/CopyRightModsWidget.java
+++ src/main/java/org/hiedacamellia/wedocopyright/client/gui/widget/CopyRightModsWidget.java
@@ -48,8 +48,8 @@ public final class CopyRightModsWidget extends CopyRightWidget {
             position.y += y;
         }
 
-        public void draw(GuiGraphics guiGraphics){
-            guiGraphics.blit(texture, position.x, position.y, MAX_HEIGHT *size.width/size.height,MAX_HEIGHT,0,0, size.width, size.height,size.width, size.height);
+        public void draw(GuiGraphics guiGraphics) {
+            guiGraphics.blit(texture, position.x, position.y, MAX_HEIGHT * size.width / size.height, MAX_HEIGHT, 0, 0, size.width, size.height, size.width, size.height);
         }
     }
 
@@ -59,36 +59,40 @@ public final class CopyRightModsWidget extends CopyRightWidget {
         int guiScaledHeight = window.getGuiScaledHeight();
         List<Pair<ResourceLocation, Size2i>> logos = new ArrayList<>();
         FMLLoader.getLoadingModList().getMods().forEach(modInfo -> {
-                    Pair<ResourceLocation, Size2i> pair = modInfo.getLogoFile().map(logoFile ->
-                    {
-                        if(modInfo.getModId().contains("fabric")&& CRClientConfig.IgnoreFabricApi.get()) {
-                            return Pair.<ResourceLocation, Size2i>of(null, new Size2i(0, 0));
-                        }
-                        TextureManager tm = Minecraft.getInstance().getTextureManager();
-                        final Pack.ResourcesSupplier resourcePack = ResourcePackLoader.getPackFor(modInfo.getModId())
-                                .orElse(ResourcePackLoader.getPackFor("neoforge").orElseThrow(() -> new RuntimeException("Can't find neoforge, WHAT!")));
-                        try (PackResources packResources = resourcePack.openPrimary(new PackLocationInfo("mod/" + modInfo.getModId(), Component.empty(), PackSource.BUILT_IN, Optional.empty()))) {
-                            NativeImage logo = null;
-                            IoSupplier<InputStream> logoResource = packResources.getRootResource(logoFile.split("[/\\\\]"));
-                            if (logoResource != null)
-                                logo = NativeImage.read(logoResource.get());
-                            if (logo != null) {
-                                return Pair.of(tm.register("modlogo", new DynamicTexture(logo) {
-                                    @Override
-                                    public void upload() {
-                                        this.bind();
-                                        NativeImage td = this.getPixels();
-                                        // Use custom "blur" value which controls texture filtering (nearest-neighbor vs linear)
-                                        this.getPixels().upload(0, 0, 0, 0, 0, td.getWidth(), td.getHeight(), modInfo.getLogoBlur(), false, false, false);
-                                    }
-                                }), new Size2i(logo.getWidth(), logo.getHeight()));
+                    try{
+                        Pair<ResourceLocation, Size2i> pair = modInfo.getLogoFile().map(logoFile ->
+                        {
+                            if (modInfo.getModId().contains("fabric") && CRClientConfig.IgnoreFabricApi.get()) {
+                                return Pair.<ResourceLocation, Size2i>of(null, new Size2i(0, 0));
                             }
-                        } catch (IOException ignored) {
+                            TextureManager tm = Minecraft.getInstance().getTextureManager();
+                            final Pack.ResourcesSupplier resourcePack = ResourcePackLoader.getPackFor(modInfo.getModId())
+                                    .orElse(ResourcePackLoader.getPackFor("neoforge").orElseThrow(() -> new RuntimeException("Can't find neoforge, WHAT!")));
+                            try (PackResources packResources = resourcePack.openPrimary(new PackLocationInfo("mod/" + modInfo.getModId(), Component.empty(), PackSource.BUILT_IN, Optional.empty()))) {
+                                NativeImage logo = null;
+                                IoSupplier<InputStream> logoResource = packResources.getRootResource(logoFile.split("[/\\\\]"));
+                                if (logoResource != null)
+                                    logo = NativeImage.read(logoResource.get());
+                                if (logo != null) {
+                                    return Pair.of(tm.register("modlogo", new DynamicTexture(logo) {
+                                        @Override
+                                        public void upload() {
+                                            this.bind();
+                                            NativeImage td = this.getPixels();
+                                            // Use custom "blur" value which controls texture filtering (nearest-neighbor vs linear)
+                                            this.getPixels().upload(0, 0, 0, 0, 0, td.getWidth(), td.getHeight(), modInfo.getLogoBlur(), false, false, false);
+                                        }
+                                    }), new Size2i(logo.getWidth(), logo.getHeight()));
+                                }
+                            } catch (IOException ignored) {
+                            }
+                            return Pair.<ResourceLocation, Size2i>of(null, new Size2i(0, 0));
+                        }).orElse(Pair.of(null, new Size2i(0, 0)));
+                        if (pair.getLeft() != null) {
+                            logos.add(pair);
                         }
-                        return Pair.<ResourceLocation, Size2i>of(null, new Size2i(0, 0));
-                    }).orElse(Pair.of(null, new Size2i(0, 0)));
-                    if(pair.getLeft() != null) {
-                        logos.add(pair);
+                    } catch (Exception ignored) {
+                        WeDoCopyRight.LOGGER.error("Error loading logo for mod " + modInfo.getModId() + ": " + ignored.getMessage());
                     }
                 }
         );
@@ -101,43 +105,43 @@ public final class CopyRightModsWidget extends CopyRightWidget {
         return new CopyRightModsWidget(0, 0, guiScaledWidth, guiScaledHeight, Component.empty(), all);
     }
 
-    private static List<Logo> cal(List<Pair<ResourceLocation, Size2i>> logos,int guiScaledWidth,int guiScaledHeight){
-        int w=0;
-        int h=0;
+    private static List<Logo> cal(List<Pair<ResourceLocation, Size2i>> logos, int guiScaledWidth, int guiScaledHeight) {
+        int w = 0;
+        int h = 0;
         List<Logo> all = new ArrayList<>();
         List<Logo> line = new ArrayList<>();
         for (int i = 0; i < logos.size(); i++) {
             ResourceLocation left = logos.get(i).getLeft();
             Size2i size = logos.get(i).getRight();
-            int logowidth = MAX_HEIGHT*size.width/size.height ;
-            if(logowidth+w<=guiScaledWidth){
+            int logowidth = MAX_HEIGHT * size.width / size.height;
+            if (logowidth + w <= guiScaledWidth) {
                 line.add(new Logo(left, size, new Vector2i(w, h)));
-                w+=logowidth;
-            }else{
-                int offset = (guiScaledWidth-w)/2;
+                w += logowidth;
+            } else {
+                int offset = (guiScaledWidth - w) / 2;
                 line.forEach(offsetLogo -> offsetLogo.offsetX(offset));
                 all.addAll(line);
                 line.clear();
-                w=0;
-                h+=MAX_HEIGHT;
+                w = 0;
+                h += MAX_HEIGHT;
                 line.add(new Logo(left, size, new Vector2i(w, h)));
-                w+=logowidth;
+                w += logowidth;
             }
         }
-        if(!line.isEmpty()){
-            int offset = (guiScaledWidth-w)/2;
+        if (!line.isEmpty()) {
+            int offset = (guiScaledWidth - w) / 2;
             line.forEach(offsetLogo -> offsetLogo.offsetX(offset));
             all.addAll(line);
         }
-        if(h+MAX_HEIGHT>guiScaledHeight){
-            MAX_HEIGHT-=5;
-            if(MAX_HEIGHT<=0){
+        if (h + MAX_HEIGHT > guiScaledHeight) {
+            MAX_HEIGHT -= 5;
+            if (MAX_HEIGHT <= 0) {
                 WeDoCopyRight.LOGGER.error("Can't fit all logos in the screen");
                 return all;
             }
             return null;
-        }else{
-            int offset = (guiScaledHeight-h)/2 - MAX_HEIGHT/2;
+        } else {
+            int offset = (guiScaledHeight - h) / 2 - MAX_HEIGHT / 2;
             all.forEach(offsetLogo -> offsetLogo.offsetY(offset));
         }
         return all;
